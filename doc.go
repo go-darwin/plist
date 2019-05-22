c48aa64feecd5b3b24e45e84061e0c8c8de6b5b4
Koichi Shiraishi <zchee.io@gmail.com>
2019-05-22 18:55:12 +0900

    all: add canonical import path

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2019 The go-darwin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package plist implements parsing of Apple plist files.
+package plist // import "go-darwin.dev/plist"
